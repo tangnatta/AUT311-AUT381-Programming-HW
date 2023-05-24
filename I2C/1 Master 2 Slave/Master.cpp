bc726316d766f4f9b57a270fb613f08bba35506e
Nattavee Sunitsakul <87492657+tangnatta@users.noreply.github.com>
2023-05-24 11:38:35 +0700

    Create Master.cpp

---
+++ I2C/1 Master 2 Slave/Master.cpp
@@ -0,0 +1,24 @@
+// Master
+
+#include <Arduino.h>
+#include <Wire.h>
+
+#define pushButton A0
+
+int buttonState = 0;
+
+void setup()
+{
+  pinMode(pushButton, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Wire.begin();
+}
+
+void loop()
+{
+  Wire.beginTransmission(0x16);
+  buttonState = digitalRead(pushButton);
+  Wire.write(buttonState);
+  Wire.endTransmission();
+  delay(100);
+}
